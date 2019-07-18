7b613be293cd566cac9155e7e48bc521c49309cc
andyrewco <34322878+andyrewco@users.noreply.github.com>
2019-07-17 20:48:53 -0400

    Added an accuracy test for randomized data

--- single_nn.py
+++ single_nn.py
@@ -1,23 +1,28 @@
-import numpy as np
 import random as rand
 
 
-def sigmoid(x):
-	return np.exp(x)/(np.exp(x)+1)
+'''
+Name: Andrew Cohen
+Date: 7/17
+File Name: single_nn.py
+'''
 
-def sigmoidDerivative(x):
-	return x * (1-x)
-
+#Create a simple activation function
 def sign(x):
 	if x >= 0:
 		return 1
 	else:
 		return -1
+
+
+#Organize the neural net with a class structure
 class NeueralNet():
 	def __init__(self):
 		self.weights = [rand.uniform(-1,1)] * 2
 
-	def guess(self):
+	def guess(self,x):
+		self.x = x
+		self.lengthx = len(x)
 		self.sum = 0
 		for i in range(self.lengthx):
 			self.sum += self.x[i] * self.weights[i]
@@ -29,7 +34,7 @@ class NeueralNet():
 		self.lengthy = len(target)
 		self.x = x
 		self.y = target[0]
-		self.numberGuess = self.guess()
+		self.numberGuess = self.guess(self.x)
 		self.error = self.y - self.numberGuess
 
 		for i in range(len(self.weights)):
@@ -48,14 +53,14 @@ def main():
 			newFile.append(newLine.split(','))
 
 		NN = NeueralNet()
-		# i = 0
+		i = 0
 		probSum = 0
 
 		for line in newFile:
 
-			# i += 1
+			i += 1
 
-			inputArray = [int(line[0]),int(line[1])]
+			inputArray = [int(line[0]), int(line[1])]
 			outputArray = [int(line[2])]
 
 
@@ -67,19 +72,38 @@ def main():
 
 			NN.train(inputArray,outputArray)
 
-
 			# print('Computer Guess')
 			# print(NN.numberGuess)
 
 			# print('Success #:', i)
 
-			if int(line[2]) == NN.numberGuess:
+			if int(int(line[2])) == NN.output:
 				probSum += 1
 
-		print('Probability of Correct: ')		
+		print('Probability of Correct Using Training Data: ')		
 		print(float(probSum) / float(len(newFile)) * 100)
+			
+
+		probSum = 0
+		for i in range(100):
+			testInput = [rand.randint(1,100),rand.randint(1,100)]
+			if testInput[1] / testInput[0] >= 1:
+				testAnswer = 1
+			else:
+				testAnswer = -1
+
+			NN.guess(testInput)
+
+			# print('Computer Guess')
+			# print(NN.guess(testInput))
+			# print('Actual Answer')
+			# print(testAnswer)
+
+			if int(testAnswer) == NN.output:
+				probSum += 1
 
 
-
+		print('Probability of Correct Using Randomly Generated Data: ')		
+		print(float(probSum) / float(100) * 100)
 
 main()
